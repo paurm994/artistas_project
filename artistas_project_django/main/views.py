892b43b65c9151ed126768142315736175d02b73
Pau Roqué <paurm994@gmail.com>
2024-08-15 15:25:30 +0200

    crear archivos template y añadir vistas

--- artistas_project_django/main/views.py
+++ artistas_project_django/main/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def galeria(request):
+    return render(request, 'galeria.html')
+
+def registro(request):
+    return render(request, 'registro.html')
+
+def perfil(request):
+    return render(request, 'perfil.html')
+
+def detalle_obra(request, id):
+    return render(request, 'detalle_obra.html', {'id':id})
